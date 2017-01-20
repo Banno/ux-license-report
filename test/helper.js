0e4eb89f8913bd0b310d59dbe312f0887f12234a
Eric Heikes <eric.heikes@banno.com>
2017-01-20 16:51:41 -0600

    Move the custom Jasmine matcher into a function by itself.

--- test/helper.js
+++ test/helper.js
@@ -9,29 +9,29 @@ let getFixture = (filename) => {
 	return fs.readFileSync(filename, 'utf8');
 };
 
-// Custom Jasmine matchers
-let matchers = {
-	toBeReport: (util, customEqualityTesters) => {
-		return {
-			compare: (actual) => {
-				let result = { pass: true };
-				if (!actual) {
-					result.pass = false;
-				} else {
-					if (!(actual.toString instanceof Function)) { result.pass = false; }
-					if (!(actual.warnings instanceof Array)) { result.pass = false; }
-					if (!(actual.write instanceof Function)) { result.pass = false; }
-				}
-				if (result.pass) {
-					result.message = 'Expected ' + JSON.stringify(actual) + ' to not be a report object';
-				} else {
-					result.message = 'Expected ' + JSON.stringify(actual) + ' to be a report object';
-				}
-				return result;
+// Custom Jasmine matchers to check if the value is a Report object.
+let toBeReport = (util, customEqualityTesters) => {
+	return {
+		compare: (actual) => {
+			let result = { pass: true };
+			if (!actual) {
+				result.pass = false;
+			} else {
+				if (!(actual.toString instanceof Function)) { result.pass = false; }
+				if (!(actual.warnings instanceof Array)) { result.pass = false; }
+				if (!(actual.write instanceof Function)) { result.pass = false; }
 			}
-		};
-	}
+			if (result.pass) {
+				result.message = 'Expected ' + JSON.stringify(actual) + ' to not be a report object';
+			} else {
+				result.message = 'Expected ' + JSON.stringify(actual) + ' to be a report object';
+			}
+			return result;
+		}
+	};
 };
 
-exports.customMatchers = matchers;
+exports.customMatchers = {
+	toBeReport: toBeReport
+};
 exports.getFixture = getFixture;
