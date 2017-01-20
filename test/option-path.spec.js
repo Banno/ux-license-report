1823a43c721d196d415687dfd4821c3f6052980f
Eric Heikes <eric.heikes@banno.com>
2017-01-20 17:12:43 -0600

    Add test for the "path" option.

---
+++ test/option-path.spec.js
@@ -0,0 +1,22 @@
+'use strict';
+describe('"path" option', () => {
+
+	const helper = require('./helper');
+	const module = require('../');
+	const path = require('path');
+
+	let report, expectedReport;
+
+	beforeAll(done => {
+		expectedReport = helper.getFixture('fake-project-report.txt');
+		module.generateReport({ path: path.resolve(__dirname, 'fixtures', 'fake-project') })
+			.then(r => { report = r; })
+			.then(done)
+			.catch(err => { console.log(err); }); // eslint-disable-line no-console
+	});
+
+	it('should use that for the root path', () => {
+		expect(report.toString()).toBe(expectedReport);
+	});
+
+});
